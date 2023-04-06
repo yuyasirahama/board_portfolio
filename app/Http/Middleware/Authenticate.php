06775e9e6b807dcdd85b9fa7f8103b701c95f21e
github yuyasirahama <yuyasirahama@gmail.com>
2023-04-07 01:34:14 +0900

    ミドルウェアの調整をしました

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('guest.user');
+        return $request->expectsJson() ? null : route('user.auth');
     }
 }
